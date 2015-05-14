50809d582dfb4c6441ab0d314dd070bbf9d8e859
Eric Bloch <eric.bloch@udemy.com>
2015-05-14 16:16:20 -0700

    Updates

--- ts.py
+++ ts.py
@@ -1,22 +1,49 @@
 #!/usr/local/bin/python 
 
+import argparse
+import csv
 import json
 import requests
 import urllib
+import sys
+
 from string import Template
 
 
+parser = argparse.ArgumentParser(description='Trello search tool.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--board', type=str, 
+                    help='Name of board', 
+                    default='Engineering: Team Web App')
+parser.add_argument('--days', type=int, 
+                    help='Number of days of history to search', 
+                    default=14)
+# FIXME: ArgumentDefaultsHelpParser doesn't do a nice job with sys.stdout arg
+parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'),
+                    help="Output file",
+                    default=sys.stdout)
+
+args = parser.parse_args()
+
 key='00018fcfa04e7a44db89b65ff9a4c7b2'
 token='80cd8c6430aa41fefb0e60f1cad2bd73aa53ee4116108676fd5e45dda418fd77'
-days=30
-board=urllib.quote("Engineering: Team Web App")
+days=args.days
+board=urllib.quote(args.board)
 
-t = Template('https://trello.com/1/search?key=$key&token=$token&query=board%3A"$board"%20created%3A$days%20label%3Abug&modelTypes=cards&card_fields=name,url,short_url&cards_limit=1000')
+print 'Searching' + ' "' + args.board + '" ' + str(days) + ' days back'
+
+t = Template('https://trello.com/1/search?key=$key&token=$token&query=board%3A"$board"%20created%3A$days%20label%3Abug&modelTypes=cards&card_fields=idShort,name,closed,url,shortUrl&cards_limit=1000')
 
 url = t.substitute(key=key, token=token, days=days, board=board)
 
 r = requests.get(url)
 
-for card in r.json()['cards'] :
-    print card['name'], ',', card['url']
+with args.outfile as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for card in r.json()['cards'] :
+        c = []
+        for key, value in card.iteritems():
+            c.append(value)
+        writer.writerow(c)
 
