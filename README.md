7130da2105a6e5212656c97496176967cae395ab
Eric Bloch <eric.bloch@udemy.com>
2015-07-07 19:19:24 -0700

    Add a.py and update the README

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # trello-fun
 
-Some tools for working with Trello API
+Trello utilities built using the Trello REST API.  They read credentials from `keys.yml` of the form:
+
+     Key: xxxxxxxxx
+     Secret: yyyyyyyyyyyyy
+     Token: xxxxxxxxxxxxxxxxxxxx
+
+See Trello API authentication details.  The key and secret are used for read only activities.
+The token needs to be a read/write capable token and is only needed and used by the am.py utility
+
+
+
+## Assign Members 
+
+    ./am.py -h
+
+Reads a hardcoded board ID and copies all users and admins from that board to the specified board.
+
+## Trello Search
+
+Basic trello search tool.
+
+    ./ts.py -h
---
+++ am.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python 
+
+import argparse
+import csv
+import json
+import requests
+import urllib
+import sys
+import yaml
+
+from string import Template
+
+
+parser = argparse.ArgumentParser(description='Trello board membership tool.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--dryrun', action='store_true', 
+                    help='Just list the users', 
+                    default=False)
+parser.add_argument('--boardID', type=str, 
+                    help='Board ID', 
+                    default='bdpGLeHh') # Trello Testing Sandbox board
+
+args = parser.parse_args()
+
+sourceBoardID='l2f4QHJ8'  #webapp board hardcoded for now
+
+keys = yaml.load(open("keys.yml", 'r'))
+key= keys['Key']
+secret = keys['Secret']
+token = keys['Token']
+boardID=urllib.quote(args.boardID)
+
+print 'Adding members to' + ' "' + args.boardID + '" from "' + sourceBoardID + '"'
+
+t = Template('https://trello.com/1/board/$sourceBoardID/members?filter=admins&key=$key&token=$token')
+url = t.substitute(key=key, token=secret, sourceBoardID=sourceBoardID)
+admins = requests.get(url).json()
+
+t = Template('https://trello.com/1/board/$sourceBoardID/members?filter=normal&key=$key&token=$token')
+url = t.substitute(key=key, token=secret, sourceBoardID=sourceBoardID)
+users = requests.get(url).json()
+
+print 'Admins'
+for admin in admins:
+    print ' ' + admin[u'username']
+    if args.dryrun:
+        continue
+    t = Template('https://trello.com/1/board/$boardID/members/$member?key=$key&token=$token&type=admin')
+    url = t.substitute(key=key, token=token, boardID=boardID, member=admin[u'id'])
+    r = requests.put(url)
+    if r.status_code != 200:
+        print r
+        sys.exit
+    
+print 'Normal Users'
+for user in users:
+    print ' ' + user[u'username']
+    if args.dryrun:
+        continue
+    t = Template('https://trello.com/1/board/$boardID/members/$member?key=$key&token=$token&type=normal')
+    url = t.substitute(key=key, token=token, boardID=boardID, member=user[u'id'])
+    r = requests.put(url)
+    if r.status_code != 200:
+        print r
+        sys.exit
